47bcd464ece815c3002eb1f3961e1bcdbd6f516b
raos-projects <8109740+raos-projects@users.noreply.github.com>
2025-06-11 00:26:11 -0500

    removed `.id` argument and completed implementation of `oss`

--- R/oss.R
+++ R/oss.R
@@ -1,7 +1,7 @@
 usethis::use_package("dplyr")
 usethis::use_package('magrittr')
 usethis::use_package('future')
-usethis::use_package('furrr')
+usethis::use_package('future.apply')
 
 #' Operative Stress Score
 #'
@@ -27,10 +27,6 @@ usethis::use_package('furrr')
 #' include [opstress::example_cpts_long] and [opstress::example_cpts_wide]
 #' @param .column_prefix prefix in names of columns which contain CPT codes to
 #' be assigned OSS values (e.g. 'procd' for `procd1`, `procd2`, etc.)
-#' @param .id name of column containing patient or encounter IDs. Default
-#' value `NA` assumes no ID column and that each row refers to a unique unit. If
-#' value is a vector of column names, then all unique combinations of values from
-#' the columns will be used as the grouping criterion (similar to [dplyr::group_by])
 #' @param .ncores the number of cores to use in parallel processing when generating
 #' OSS columns for each column of CPT codes
 #'
@@ -39,38 +35,52 @@ usethis::use_package('furrr')
 #' @export
 #'
 #' @examples oss(example_cpts_wide, 'procd') #no need to specify .id given each row is assumed to represent a unique unit
-#' @examples oss(example_cpts_long, 'procd', .id = 'mrn') #multiple rows with same value of `mrn` all specify the same unit
-#' @examples oss(example_cpts_long, 'procd', .id = 'mrn', .ncores = 4) #compute OSS in parallel across 4 cores
+#' @examples oss(example_cpts_long, 'procd') #multiple rows with same value of `mrn` all specify the same unit
+#' @examples oss(example_cpts_long, 'procd', .ncores = 4) #compute OSS in parallel across 4 cores
 #'
-oss <- function(.data, .column_prefix, .id = NA, .ncores = 1){
 
-  warning('`oss` is an incomplete function')
+oss <- function(.data, .column_prefix = NA, .ncores = 1) {
 
-  #determine whether data is wide (multiple cpt columns) or long (one cpt column)
-  columns = select(.data, dplyr::starts_with(.column_prefix))
-  column_names = names(columns)
-  column_types = lapply(columns, typeof)
-
-  secret_match_oss <- function(column_) {
-    column_name_ = names(column_)
-    names(column_) <- 'Input_CPT'
-    column_with_oss_ <- dplyr::left_join(data.frame(column_),
-                                         dplyr::select(cpt_table,
-                                                       c(CPTcode,Consensus.OSS)),
-                                         by=join_by('Input_CPT' == 'CPTcode'))
-    # column_with_oss_ = dplyr::rename_with(column_with_oss_,
-    #                                       ~ paste0(column_name_,'_oss'),
-    #                                       dplyr::starts_with('Consensus.OSS'))
-    names(column_with_oss_) <- c(column_name_, paste0(column_name_, '_oss'))
-    return(column_with_oss_)
+  if(is.matrix(.data)){
+    .data = as.data.frame(.data)
   }
 
-  plan(multicore, workers = .ncores)
-  ossified_columns <- furrr::future_pmap(.l = list(column_ = as.list(columns)),
-                                         .f = secret_match_oss)
-  ossified_columns <- cbind(ossified_columns)
-  select
+  if(is.vector(.data)){
+    if(is.null(names(.data)) && is.na(.column_prefix)){
+      .data = list('dx' = .data)
+      .column_prefix = 'dx'
+    } else if(length(names(.data)) == 1 && !(.column_prefix %in% names(.data))) {
+      warning(paste('argument `.data` is a vector, but `names(.data)` is not equivalent to argument `.column_prefix` (',.column_prefix,'). Will ignore `.column_prefix`'))
+      .column_prefix = names(.data)
+    }
+  } else if (is.list(.data) && !(.column_prefix %in% names(.data))) {
+    if(length(names(.data)) == 1){
+      warning(paste('argument `.data` is a list or data.frame, but `names(.data)` is not equivalent to argument `.column_prefix` (',.column_prefix,'). Will ignore `.column_prefix`'))
+      .column_prefix = names(.data)
+    } else if(length(names(.data)) > 1 && !(.column_prefix %in% names(.data))) {
+      stop(paste('argument `.column_prefix` (',.column_prefix,') is not found in `names(.data)`'))
+    }
+  }
 
+  cpt_map <- opstress::cpt %>%
+    dplyr::select(cpt_code, oss)
 
-  return(.data)
+  cpt_cols <- grep(paste0("^", .column_prefix), names(.data), value = TRUE)
+
+  get_oss_col <- function(col) {
+    cpt_vec <- .data[[col]]
+    oss_vec <- cpt_map$oss[match(cpt_vec, cpt_map$cpt_code)]
+    oss_vec
+  }
+
+  if (.ncores > 1) {
+    future::plan(future::multisession, workers = .ncores)
+    oss_matrix <- future.apply::future_lapply(cpt_cols, get_oss_col) %>% as.data.frame()
+    future::plan(future::sequential)  # Reset to sequential to avoid surprises later
+  } else {
+    oss_matrix <- lapply(cpt_cols, get_oss_col) %>% as.data.frame()
+  }
+
+  names(oss_matrix) <- paste0(cpt_cols, "_oss")
+  return(bind_cols(.data, oss_matrix))
 }
