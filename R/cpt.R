b1af5a51819910963ce5766654d619e22c45b269
raos-projects <8109740+raos-projects@users.noreply.github.com>
2025-06-11 01:32:49 -0500

    Added `example_cpts` and updated LICENSE and citation for `cpt.R`

--- R/cpt.R
+++ R/cpt.R
@@ -5,5 +5,5 @@
 #' matching CPT codes to OSS in the [opstress::oss()] function.
 #'
 #' @format three columns: `cpt_code<integer>`, `cpt_description<character>`, and `oss<integer>`
-#' @source From George EL, Hall DE, Youk A, Chen R, Kashikar A, Trickey AW, Varley PR, Shireman PK, Shinall MC, Massarweh NN, Johanning J. Association between patient frailty and postoperative mortality across multiple noncardiac surgical specialties. JAMA surgery. 2021 Jan 1;156(1):e205152-. List obtained from `sarya1 AT stanford DOT edu` and dated 2019-11-14
+#' @source From Shinall MC, Arya S, Youk A, et al. Association of Preoperative Patient Frailty and Operative Stress With Postoperative Mortality. JAMA Surg. 2020;155(1):e194620. doi:10.1001/jamasurg.2019.4620. Table obtained from `sarya1 AT stanford DOT edu` and dated 2019-11-14
 "cpt"
---
+++ R/example_cpts.R
@@ -0,0 +1,7 @@
+#' Example CPT Codes
+#'
+#' A data frame containing example CPT codes, including invalid codes not
+#' present in [opstress::cpt]. This table is used for the examples only.
+#'
+#' @format one column: `procd<integer>`
+"example_cpts"
